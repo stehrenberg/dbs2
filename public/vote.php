22e0efabf0d5ac1da6cd4357611377b617abd295
stehrenberg <sehrenbe@hm.edu>
2016-10-18 01:14:54 +0200

    WIP creating new user in db on vote submission

--- public/vote.php
+++ public/vote.php
@@ -8,20 +8,46 @@
 require_once __DIR__ . '/../backend/DatabaseConnector.php';
 
 $formData = $_POST;
+$userName = $formData["name"];
 $votedClubId = $formData["clubs"];
 $userEmail = $formData["email"];
 
 $dbConnector = new DatabaseConnector();
 $dbConn = $dbConnector->getConnection();
 
-$stmt = $dbConn->prepare('SELECT * FROM user where email = :email');
-$stmt->bindParam(':email', $userEmail);
-$stmt->execute();
+if(!doesUserExist($userEmail, $dbConn)) {
+    $userId = createUser($userName, $userEmail, $dbConn);
 
-if (count($stmt->fetchAll()) == 0) {
-    printf("User %s does not exist!", $userEmail);
+    if(!hasUserVotedYet($userId, $dbConn)) {
+        printf("user %s with ID %i has not voted yet. Creating entry...", $userEmail, $userId);
+        //countVote($userId, $votedClubId, $dbConn);
+    }
 }
 
-echo "bla";
+function doesUserExist($email, $dbConn) {
+    return count(findUserByEmail($email, $dbConn)) > 0;
+}
 
-//$queryResult = $dbConn->query("INSERT INTO votes VALUES($votedClubId, $user)");
+function findUserByEmail($email, $dbConn) {
+    $stmt = $dbConn->prepare('SELECT * FROM user WHERE email = :email');
+    $stmt->bindParam(':email', $email);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function createUser($name, $email, $dbConn) {
+    $stmt = $dbConn->prepare('INSERT INTO user VALUES(NULL, :name, :email)');
+    $stmt->bindParam(':name', $name);
+    $stmt->bindParam(':email', $email);
+    $stmt->execute();
+    $userId = findUserByEmail($email, $dbConn)[0]['user_id'];
+    var_dump($userId);
+    return $userId;
+}
+
+function hasUserVotedYet($userId, $dbConn) {
+    $stmt = $dbConn->prepare('SELECT * FROM Votes WHERE Votes.user_id = :userId');
+    $stmt->bindParam(':userId', $userId);
+    $stmt->execute();
+    return count($stmt->fetchAll()) > 0;
+}
