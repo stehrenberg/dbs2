2c0faab7ee5128b7d91eda06737088450c7a272d
stehrenberg <sehrenbe@hm.edu>
2016-10-17 02:31:17 +0200

    WIP - transmit form data which must now include an email address and insert vote result and mailadd into database

--- public/vote.php
+++ public/vote.php
@@ -5,3 +5,21 @@
  * Date: 17/10/16
  */
 
+require_once __DIR__ . '/../backend/DatabaseConnector.php';
+
+$formData = $_POST;
+$votedClubId = $formData["clubs"];
+$userEmail = $formData["email"];
+
+$dbConnector = new DatabaseConnector();
+$dbConn = $dbConnector->getConnection();
+
+$stmt = $dbConn->prepare('SELECT * FROM user where email = :email');
+$stmt->bindParam(':email', $userEmail);
+$stmt->execute();
+
+if (count($stmt->fetchAll()) == 0) {
+    printf("User %s does not exist!", $userEmail);
+}
+
+//$queryResult = $dbConn->query("INSERT INTO votes VALUES($votedClubId, $user)");
