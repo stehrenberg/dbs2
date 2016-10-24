3e52b3260676fa48f6b2d41fc97cb7068289510b
stehrenberg <sehrenbe@hm.edu>
2016-10-24 18:57:59 +0200

    Fix templating bug with multiselect tool

--- public/assets/js/multiselectHandler.js
+++ public/assets/js/multiselectHandler.js
@@ -1,46 +1,44 @@
 /**
  * Created by Steff on 24/10/16.
  */
-$(function () {
+$(document).ready(function () {
 
     var templateScript = $("#select_news_cat-template").html();
     var template = Handlebars.compile(templateScript);
     var context = {
-        newsCategories: []
-    };
-
-    //compileTemplate(context);
-    //FIXME Wenn Ajax-Call drin muss der Ablauf wieder geändert werden!
-    //getNewsCatList();
-
-    var theCompiledHtml = template(getNewsCatList());
-    var newsCategories = $('#news-categories');
-    newsCategories.html(theCompiledHtml);
-    var newsCatMultiSelect = $('#news-categories');
-    newsCatMultiSelect.multiselect();
-
-    function getNewsCatList() {
-        return context.newsCategories = [
+        newsCategories: [
             {
                 categoryId: "0",
-                categoryName: "Test",
+                name: "Test",
             },
             {
                 categoryId: "1",
-                categoryName: "Test2",
-            }];
-        // $.ajax({
-        //     method: 'GET',
-        //     url: 'listclubs.php',
-        //     dataType: 'json',
-        //     success: function (data) {
-        //         context.newsCategories = data;
-        //         compileTemplate(context);
-        //     },
-        //     error: function (jqXHR, textStatus, error) {
-        //         console.log(error);
-        //     }
-        // });
+                name: "Test2",
+            }]
+    };
+
+    //FIXME Wenn Ajax-Call drin muss der Ablauf wieder geändert werden!
+    //compileTemplate(context);
+    //getNewsCatList();
+
+    var theCompiledHtml = template(context);
+    var newsCategories = $('#news-categories');
+    newsCategories.html(theCompiledHtml);
+    newsCategories.multiselect();
+
+    function getNewsCatList() {
+        $.ajax({
+            method: 'GET',
+            url: 'listclubs.php',
+            dataType: 'json',
+            success: function (data) {
+                context.newsCategories = data;
+                compileTemplate(context);
+            },
+            error: function (jqXHR, textStatus, error) {
+                console.log(error);
+            }
+        });
     }
 
     function compileTemplate(context) {
---
+++ public/newsletterCategories.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Steff
+ * Date: 24/10/16
+ * Time: 18:56
+ */
