4bac87c36e7480371de4500caf516d0592338f56
stehrenberg <sehrenbe@hm.edu>
2016-11-06 15:00:57 +0100

    Extract all relevant form data for newsletter sending and use real query to get all subscribed users from database for selected news categories.

--- public/sendNewsletter.php
+++ public/sendNewsletter.php
@@ -5,11 +5,27 @@ require_once __DIR__ . '/../backend/DatabaseConnector.php';
 $formData = $_POST;
 $mailSubject = $formData["mail-subject"];
 $mailText = $formData["mailtext"];
+$newsCategories = isset($formData["news"]) ? $formData["news"] : null;
+$newsCatsAsString = implode(', ', $newsCategories);
 $onlyClubmembers = isset($formData["for-club-member"]);
+
 
 $dbConnector = new DatabaseConnector();
 $dbConn = $dbConnector->getConnection();
-$queryResult = $dbConn->query("SELECT name, email FROM User")->fetchAll(PDO::FETCH_ASSOC);
+
+$stmt = $dbConn->prepare("
+    SELECT name, email, News.newsletter_id 
+    FROM User, Newsletter_votes AS News 
+    WHERE User.user_id = News.user_id 
+    AND News.newsletter_id IN (:newsCats)
+    AND is_member = :isMember;
+");
+$stmt->bindParam(':newsCats', $newsCatsAsString);
+$stmt->bindParam(':isMember', $onlyClubmembers);
+$stmt->execute();
+
+$queryResult = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 $emailData = [
     "subject" => $mailSubject,
     "text" => $mailText,
